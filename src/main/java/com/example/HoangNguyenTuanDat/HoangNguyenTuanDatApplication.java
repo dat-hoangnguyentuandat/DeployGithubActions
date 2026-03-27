c8433ad5f105641a912573a1e157f6f6030948f8
Tuấn Đạt <hoangdatlnbp@gmail.com>
2026-03-27 08:59:53 +0700

    first commit

---
+++ src/main/java/com/example/HoangNguyenTuanDat/HoangNguyenTuanDatApplication.java
@@ -0,0 +1,13 @@
+package com.example.HoangNguyenTuanDat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoangNguyenTuanDatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoangNguyenTuanDatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HoangNguyenTuanDat/HoangNguyenTuanDatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HoangNguyenTuanDat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoangNguyenTuanDatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
